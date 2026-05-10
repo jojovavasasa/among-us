6291f3be27da61a205ed445c67a88f80db2e7f2c
jojovavasasa <106984298+jojovavasasa@users.noreply.github.com>
2026-05-10 17:14:01 +0200

    add tasks

--- src/gamevariables.js
+++ src/gamevariables.js
@@ -1,19 +1,14 @@
 const TASKS = [
-	"Do 10 reps of machine exercise (Joe's Gym)",
-	'Pour water (Kitchen)',
-	'Sink 1 ball (Billards table)',
-	"Flip water bottle (Michael's room)",
-	'Wash your hands (basement bathroom)',
-	'Wash your hands (1st floor bathroom)',
-	'Take elevator',
-	'Spin 8, 9, or 10 in Life game (Hearth room)',
-	'Beat Smash (Upstairs guest room)',
-	'Hit a layup (Basketball court)',
-	'Take photo (Green screen)',
-	// 'Mess with Jack (basement)',
-	'Bounce ping pong ball 10 times (front door)',
-	'Take a lap (Around pool)',
-	'Flip a pillow (Activity room)'
+	'Was handen (wc boven)',
+	'Ga op en neer trap (trap)',
+	'Teken iets leuk (whiteboard papegaaienkamer)',
+	'Doe een dutje onder de pooltafel (Meme en Pepe slaapkamer)',
+	'Zwaai naar een voetganer en laat hun atwoorden (Meme en Pepe slaapkamer)',
+	'Drink water (keuken)',
+	'Ga naar de toilet (wc)',
+	'Leg het bed goed (Meme en Pepe slaapkamer rechts)',
+	'Leg het bed goed (papegaaienkamer)',
+	'Leg het bed goed (Meme en Pepe slaapkamer links)',
 ];
 const N_TASKS = 5;
 const N_IMPOSTORS = 1;
