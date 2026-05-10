7c4331ad7fb327d0288866697e71265d3b848fe2
jojovavasasa <106984298+jojovavasasa@users.noreply.github.com>
2026-05-10 15:24:11 +0200

    Seperate frequently changable variables to src/gamevariables.js

---
+++ src/gamevariables.js
@@ -0,0 +1,19 @@
+const TASKS = [
+	"Do 10 reps of machine exercise (Joe's Gym)",
+	'Pour water (Kitchen)',
+	'Sink 1 ball (Billards table)',
+	"Flip water bottle (Michael's room)",
+	'Wash your hands (basement bathroom)',
+	'Wash your hands (1st floor bathroom)',
+	'Take elevator',
+	'Spin 8, 9, or 10 in Life game (Hearth room)',
+	'Beat Smash (Upstairs guest room)',
+	'Hit a layup (Basketball court)',
+	'Take photo (Green screen)',
+	// 'Mess with Jack (basement)',
+	'Bounce ping pong ball 10 times (front door)',
+	'Take a lap (Around pool)',
+	'Flip a pillow (Activity room)'
+];
+const N_TASKS = 5;
+const N_IMPOSTORS = 1;
